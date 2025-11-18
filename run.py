d82bf6e232bd6cef105fa953935c1a12bdeb3cb5
minjunkim0205 <minjunkim0205@gmail.com>
2025-11-18 10:29:03 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,30 +1,31 @@
-import streamlit
+import streamlit as st
+
 
 # Page setting
-streamlit.set_page_config(
-    page_title="Streamlit Tutorial",
+st.set_page_config(
+    page_title="Week11주차 Streamlit CSV Read 팀 프로젝트",
     page_icon=":shark:",
     layout="wide",
     initial_sidebar_state="auto"
 )
 
 # Task 01
-streamlit.title("Task 01")
+st.title("Task 01")
 
 # Task 02
-streamlit.title("Task 02")
+st.title("Task 02")
 
 # Task 03
-streamlit.title("Task 03")
+st.title("Task 03")
 
 # Task 04
-streamlit.title("Task 04")
+st.title("Task 04")
 
 # Task 05
-streamlit.title("Task 05")
+st.title("Task 05")
 
 # Task 06
-streamlit.title("Task 06")
+st.title("Task 06")
 
 # Task 07
-streamlit.title("Task 07")
+st.title("Task 07")
