6d5809f7a383013dea6765c28bf28d0eedbc76fe
Codek327 <han183536@gmail.com>
2025-11-24 19:26:54 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 # Page setting
 st.set_page_config(
@@ -23,7 +24,7 @@ if 'color' not in st.session_state:
 
 name_input = st.text_input("이름을 입력하세요.", value=st.session_state['name'])
 age_input = st.slider("나이", min_value=0, max_value=100, value=25, key="age")
-color_input = st.st.selectbox("좋아하는 색상", ['red','orange','green','blue','violet'])
+color_input = st.selectbox("좋아하는 색상", ['red','orange','green','blue','violet'])
 
 agree = st.checkbox("이용 약관에 동의합니다.")
 
@@ -36,31 +37,31 @@ if st.button('?제출'):
 st.title("Task 02")
 
 # Task 03
-streamlit.title("Task 03")
+st.title("Task 03")
 #막대 그래프 생성
-chart_data = pandas.DataFrame(
+chart_data = pd.DataFrame(
     {
         "X 축": list(range(20)) * 3,
-        "Y 축": numpy.random.randn(60),
+        "Y 축": np.random.randn(60),
         "색": ["A"] * 20 + ["B"] * 20 + ["C"] * 20
     }
 )
 #막대 그래프 출력
-streamlit.bar_chart(chart_data, x="X 축", y="Y 축", color = "색")
+st.bar_chart(chart_data, x="X 축", y="Y 축", color = "색")
 #선 그래프 생성
-line_data = pandas.DataFrame(
-    numpy.random.randn(20, 3),
+line_data = pd.DataFrame(
+    np.random.randn(20, 3),
     columns=["A", "B", "C"]
 )
 #선 그래프 출력
-streamlit.line_chart(line_data)
+st.line_chart(line_data)
 #영역 그래프 생성
-area_data = pandas.DataFrame(
-    numpy.random.randn(20, 3),
+area_data = pd.DataFrame(
+    np.random.randn(20, 3),
     columns=["A", "B", "C"]
 )
 #영역 그래프 출력
-streamlit.area_chart(area_data)
+st.area_chart(area_data)
 
 # Task 04
 st.title("Task 04")
